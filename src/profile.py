8b92836fb3878781c0a9bee379d240d2e1253668
Димитър Тошев <mo6eeeb@gmail.com>
2010-11-24 16:29:16 +0200

    Remove prototype files

--- src/profile.py
+++
@@ -1,5 +0,0 @@
-import conceptologic_bots
-import cProfile
-import sys
-cProfile.run('conceptologic_bots.main()', sys.argv[1])
-
--- src/stacked_profile.py
+++
@@ -1,5 +0,0 @@
-import stacked
-import cProfile
-import sys
-cProfile.run('stacked.main()', sys.argv[1])
-
