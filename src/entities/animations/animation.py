3ef32f1290ef048b73e9870fccc2499552d5c8c5
Димитър Тошев <mo6eeeb@gmail.com>
2010-11-24 16:28:23 +0200

    Reorganise source.

---
+++ src/entities/animations/animation.py
@@ -0,0 +1,5 @@
+class animation:
+    def __init__(self, entity, frames, loop, parent = None):
+        '''
+        '''
+
---
+++ src/stacked_profile.py
@@ -0,0 +1,5 @@
+import stacked
+import cProfile
+import sys
+cProfile.run('stacked.main()', sys.argv[1])
+
